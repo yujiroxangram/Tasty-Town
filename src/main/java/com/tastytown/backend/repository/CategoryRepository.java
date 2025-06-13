5e1b63bfe35049eeb464be1fccab9c398ff62c36
Sangram Majhi <155440916+yujiroxangram@users.noreply.github.com>
2025-06-13 09:26:57 +0530

    Delete src directory
    deletd

--- src/main/java/com/tastytown/backend/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tastytown.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tastytown.backend.entity.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, String> {
-    
-}
--- src/main/java/com/tastytown/backend/repository/FoodRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tastytown.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tastytown.backend.entity.Food;
-
-public interface FoodRepository extends JpaRepository<Food, String> {
-
-}
